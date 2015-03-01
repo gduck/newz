13145686250e03d701e387ee18ad63bc996a482b
gduck <dale.github@thefoos.net>
2015-03-01 15:05:41 +0800

    did some funny stash popping, hoping all is well again now

---
+++ db/migrate/20150301062842_create_cards.rb
@@ -0,0 +1,15 @@
+class CreateCards < ActiveRecord::Migration
+  def change
+    create_table :cards do |t|
+
+      t.date :date
+      t.attachment :avatar
+      t.string :name
+      t.string :feature_heading
+      t.string :feature_article
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
