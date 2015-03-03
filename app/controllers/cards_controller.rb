87393f5a7421a44189b7d4c071420e642a0224cd
gduck <dale.github@thefoos.net>
2015-03-03 11:02:49 +0800

    removed unecessary files

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,26 +1,5 @@
 class CardsController < ApplicationController
 
-  def saveCard
-    @card = Card.new(card_params)
-    if @card.save
-      render json: {success: true}
-    else
-      render json: @card.errors
-    end  
-  end  
-
-  def new
-    @card = Card.new()
-    puts card_params
-    puts "*************************"
-    puts [:params]
-
-  end
-
-
-  private 
-  def card_params
-    params.require(:card).permit(:date, :image, :name, :feature_heading, :feature_article, :author)
-  end 
+  
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301070802) do
+ActiveRecord::Schema.define(version: 20150301062842) do
 
   create_table "cards", force: true do |t|
     t.date     "date"
